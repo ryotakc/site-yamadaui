fa51b18aed48a4ac0b75897406aa80ebfb47f21c
Ryota Kato <rka147@sfu.ca>
2024-06-26 10:49:22 -0700

    add Calender for demo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 //import CarouselDemo from "./components/carousel";
 import type { Metadata } from "next";
 import { AlphaSlider } from "@yamada-ui/react"
+import { Calendar } from "@yamada-ui/calendar"
 
 export const metadata: Metadata = {
     title: 'About Services',
@@ -13,6 +14,7 @@ export default function About() {
         <div className="container mx-auto space-y-5">
             <h1 className="font-bold text-2xl">About Page</h1>
             <AlphaSlider />
+            <Calendar />
         </div>
     )
 }
